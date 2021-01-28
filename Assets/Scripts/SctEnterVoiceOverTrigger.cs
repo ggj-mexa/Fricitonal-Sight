c7cbcb61e8e7c33cf0b54b364b4ca0bd3ea1972e
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-27 19:27:32 -0600

    #1.2 Fixed Audio Scripts

---
+++ Assets/Scripts/SctEnterVoiceOverTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SctEnterVoiceOverTrigger : MonoBehaviour
+{
+    public ScrVoiceOverLogic scrVoiceOverLogic;
+
+    private void OnTriggerEnter(Collider other) //el avatar juega marco polo, toco a alguien
+    {
+        if (other.GetComponent<VoiceOverTrigger>() != null)
+        //si toco a alguien que tiene el componente, signfica que es para
+        //reproducir un voice over
+        {
+            //Invocamos al voice over logic para decirle que tocamos un
+            //voice over trigger, con cierto índice
+            scrVoiceOverLogic.ReproduceVoiceOver(other.GetComponent<VoiceOverTrigger>().voiceOverIndex);
+        }
+    }
+}
