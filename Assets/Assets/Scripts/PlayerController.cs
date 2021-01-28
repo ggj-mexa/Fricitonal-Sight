7d85cf19dfb37813229d945eacec9723cac22d0d
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-28 16:30:37 -0600

    #3_Integración de ambas partes

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 5f;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y = 0;
+
+        Vector3 move = transform.right * x + transform.up * y + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Assets/Scripts/Raycaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    public LayerMask mask;
+    public Camera fpv;
+    public GameObject ui;
+
+    private string hitName;
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = fpv.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 5))
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.green);
+            //hitName = hitInfo.collider.gameObject.GetComponent<objectManager>().itemName;
+            //ui.GetComponent<UIText>().DisplayName(hitName);
+
+            //if (Input.GetMouseButtonDown(0)) { hitInfo.transform.gameObject.GetComponent<objectManager>().Interaction(); }
+        }
+        else
+        {
+            Debug.DrawRay(ray.origin, ray.direction, Color.red);
+            //hitName = "";
+            //ui.GetComponent<UIText>().DisplayName(hitName);
+        }
+    }
+}
