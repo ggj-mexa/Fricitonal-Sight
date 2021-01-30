b9b3d03d5ca7e3042fea018d837c40209c3ee270
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-30 00:13:45 -0600

    #6_2 Arreglado el tema de seleccion

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -8,6 +8,17 @@ public class Raycaster : MonoBehaviour
     public Camera fpv;
     public GameObject ui;
     RaycastHit hitInfo;
+    public string thisTag;
+    public Receiver receiver;
+    public bool disableOther;
+    public GameObject KeySound1;
+    public GameObject KeySound2;
+    public GameObject KeySound3;
+    public GameObject KeySound4;
+    public GameObject KeySound5;
+    public GameObject KeySound6;
+    public GameObject KeySound7;
+    public GameObject KeySound8;
 
     public bool flag = false;
     public VoiceOverCatalog index; 
@@ -18,20 +29,28 @@ public class Raycaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        LeftMouse();
+        RightMouse(); 
+
+
+    }
+
+
+    void LeftMouse()
+    {
         Ray ray = fpv.ScreenPointToRay(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
-
-
-            if (Physics.Raycast(ray, out hitInfo, 15) && hitInfo.transform.tag == "KeySound")
+            if (Physics.Raycast(ray, out hitInfo, 15) && hitInfo.transform.tag == "Narrator")
             {
 
                 scrVoiceOverLogic.ReproduceVoiceOver(hitInfo.transform.gameObject.GetComponent<VoiceOverTrigger>().voiceOverIndex);
-                Debug.Log("Error"); 
-              
-                if(Input.GetMouseButtonDown(0))
+                Debug.Log("Error");
+
+                if (Input.GetMouseButtonDown(0))
                 {
-                    flag = true; 
+                    flag = true;
                 }
 
                 //hitName = hitInfo.collider.gameObject.GetComponent<objectManager>().itemName;
@@ -39,23 +58,63 @@ public class Raycaster : MonoBehaviour
 
                 //if (Input.GetMouseButtonDown(0)) { hitInfo.transform.gameObject.GetComponent<objectManager>().Interaction(); }
             }
-          
         }
-       
-        if (Input.GetMouseButtonDown(1) && hitInfo.collider.tag == "KeySound")
-        {
-               
-                index = hitInfo.transform.gameObject.GetComponent<VoiceOverTrigger>().voiceOverIndex;
-                Debug.Log(index);   
-            
-        }
-
-        if (GameObject.Find("GoOverLogic").GetComponent<ScrVoiceOverLogic>().omit == true)
-        {
-            flag = false;
-        }
-        Debug.Log("Es");
-        Debug.Log(flag); 
-
     }
+
+    void RightMouse()
+    {
+        thisTag = hitInfo.transform.tag;
+        Debug.Log(thisTag); 
+            switch(thisTag)
+            {
+            case "KeySound":
+                {
+            
+                    //Si es este quiero que tome el tag, y se lo mande a receiver, para poder diferenciar 
+                    //entre los distintos objetos
+                    KeySound2.SetActive(false);
+                    receiver.RecieveOrders(thisTag);
+
+
+
+                }
+            break;
+
+            case "KeySound2":
+                {
+     
+                    //Si es este quiero que tome el tag, y se lo mande a receiver, para poder diferenciar 
+                    //entre los distintos objetos
+                    KeySound1.SetActive(false);
+                    receiver.RecieveOrders(thisTag);
+   
+
+
+                }
+
+                break;
+            }
+
+
+            if (Input.GetMouseButtonDown(1) && hitInfo.collider.tag == "KeySound")
+            {
+
+                index = hitInfo.transform.gameObject.GetComponent<VoiceOverTrigger>().voiceOverIndex;
+           
+
+            }
+            else
+            {
+
+            }
+
+            if (GameObject.Find("GoOverLogic").GetComponent<ScrVoiceOverLogic>().omit == true)
+            {
+                flag = false;
+            }
+           
+
+        
+    }
+
 }
