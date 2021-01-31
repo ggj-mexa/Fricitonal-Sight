1428824d74100c65f0bcb67c1f4e243e85543e14
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-30 19:25:11 -0600

    #7_2 Player test

--- Assets/Scripts/Receiver.cs
+++ Assets/Scripts/Receiver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video; 
 
 public class Receiver : MonoBehaviour
 {
@@ -40,18 +41,19 @@ public class Receiver : MonoBehaviour
 
     public void RecieveOrders(string tag)
     {
-        Debug.Log(tag + "Recibi tag");
+    
         switch (tag)
         {
-            case "KeySound1":
+            case "KeySound":
                 {
 
                     //Si es este quiero que tome el tag, y se lo mande a receiver, para poder diferenciar 
                     //entre los distintos objetos
+                    Debug.Log("entro el pejelagarto00000");
                     VideoA.SetActive(false);
-                    Debug.Log("Entre al KeySound, elimino el Video2");
-                
-
+                    Debug.Log("entro el pejelagarto00");
+                    Video1.GetComponent<VideoPlayer>().Play();
+                    Debug.Log("entro el pejelagarto"); 
 
                 }
                 break;
@@ -62,9 +64,6 @@ public class Receiver : MonoBehaviour
                     //Si es este quiero que tome el tag, y se lo mande a receiver, para poder diferenciar 
                     //entre los distintos objetos
                     Video1.SetActive(false);
-                    Debug.Log("Entre al KeySound 2, elimino el video 1");
-
-
 
 
                 }
@@ -74,8 +73,6 @@ public class Receiver : MonoBehaviour
             case "KeySound3":
                 {
                     VideoB.SetActive(false);
-                    Debug.Log("Entre al KeySound 3, elimino 4");
-
 
                 }
                 break;
@@ -83,50 +80,40 @@ public class Receiver : MonoBehaviour
             case "KeySound4":
                 {
                     Video2.SetActive(false);
-                    Debug.Log("Entre al KeySound 4, elimino 3");
-
-
+       
                 }
                 break;
 
             case "KeySound5":
                 {
                     VideoC.SetActive(false);
-                    Debug.Log("Entre al KeySound 6, elimino 6");
-
+                 
                 }
                 break;
 
             case "KeySound6":
                 {
                     Video3.SetActive(false);
-                    Debug.Log("Entre al KeySound 5, elimino 5");
-
+                  
                 }
                 break;
 
             case "KeySound7":
                 {
                     VideoD.SetActive(false);
-                    Debug.Log("Entre al KeySound 7, elimino 8");
-
+                   
                 }
                 break;
 
             case "KeySound8":
                 {
                     Video4.SetActive(false);
-                    Debug.Log("Entre al KeySound 7, elimino 8");
-
+                   
                 }
                 break;
 
 
         }
 
-        // void TurnOff()
-        //{
-        //  if(this.gameObject.GetComponent<VoiceOverTrigger>().v)
-        // }
     }
 }
