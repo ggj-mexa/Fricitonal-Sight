3db409da250a8cf0b414e256bfb09773f24e8d26
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-30 23:44:25 -0600

    #8_8

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -29,7 +29,7 @@ public class Raycaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
         LeftMouse();
         RightMouse(); 
 
