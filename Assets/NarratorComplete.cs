2bd6b57cf847800a21c29e7b3bc7e6b08c42c8b8
Alzound <61034726+Alzound@users.noreply.github.com>
2021-01-31 11:31:15 -0600

    #10_Tutorial complete

--- Assets/NarratorComplete.cs
+++ Assets/NarratorComplete.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class NarratorComplete : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(myFunction());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator myFunction()
     {
+
         
+  
+
+
+        yield return new WaitForSeconds(103);
+
+        //The scene changes to the credits. 
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        yield return null;
+
     }
 }
